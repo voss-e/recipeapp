d96255f9d0535343afaedb40d254d98c06d2307b
voss-e <jovicevicdev@gmail.com>
2019-08-21 15:40:43 +0200

    Updates

---
+++ precache-manifest.7dc9d4776546f894336e161b644a81f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf9d6600d09f0596a560b5c96445e7d",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "3d02fd413505880de9b9",
+    "url": "/recipeapp/static/css/main.4097f0ff.chunk.css"
+  },
+  {
+    "revision": "a84918c0c32c0566503b",
+    "url": "/recipeapp/static/js/2.bd0f3d3c.chunk.js"
+  },
+  {
+    "revision": "3d02fd413505880de9b9",
+    "url": "/recipeapp/static/js/main.690de3e8.chunk.js"
+  },
+  {
+    "revision": "9b712747a85498bd57e7",
+    "url": "/recipeapp/static/js/runtime~main.13029679.js"
+  }
+]);
